ad4f973d3dde63695d908fba701f4e4bbef2fbcc
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-16 17:34:39 +0545

    Web-cams

--- openCV-1.py
+++ openCV-1.py
@@ -3,7 +3,20 @@ print(cv2.__version__)
 cam = cv2.VideoCapture(0)
 while True:
     ignore, frame= cam.read()
-    cv2.imshow('my-webcam',frame)
+    grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('my-webcam',grayFrame)
+    cv2.moveWindow("my-webcam",10,10)
+    if cv2.waitKey(1) & 0xff==ord('q'):
+        break
+cam.release()
+
+
+cam1 = cv2.VideoCapture(0)
+while True:
+    ignore, frame= cam1.read()
+    grayFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    cv2.imshow('my-webcam',grayFrame)
+    cv2.moveWindow("my-webcam",100,100)
     if cv2.waitKey(1) & 0xff==ord('q'):
         break
 cam.release()
