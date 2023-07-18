19d6976629cf8a6f06cf7f2770835d1c500a19d7
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-18 16:26:42 +0545

    openCV

--- openCV-3.py
+++ openCV-3.py
@@ -6,6 +6,9 @@ while True:
     ignore, frame = cam.read()
     cv2.imshow('My-webcam', frame)
     cv2.moveWindow('My-webcam',100,100)
+    grayCam = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray-cam',grayCam)
+    cv2.moveWindow('gray-cam',300,300)
     if cv2.waitKey(1) & 0xff==ord('q'):
         break
 cam.release()
