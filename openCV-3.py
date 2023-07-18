cf1d30f20886b94241cb42d32a91532ba89c826a
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-18 16:56:33 +0545

    Faster Webcam with OpenCV

--- openCV-3.py
+++ openCV-3.py
@@ -1,14 +1,17 @@
 import cv2
-
-cam = cv2.VideoCapture(0)
+width = 460
+height=340
+cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+cam.set(cv2.CAP_PROP_FPS,30)
+cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(* 'MJPG'))
 
 while True:
     ignore, frame = cam.read()
     cv2.imshow('My-webcam', frame)
     cv2.moveWindow('My-webcam',100,100)
-    grayCam = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('gray-cam',grayCam)
-    cv2.moveWindow('gray-cam',300,300)
+    
     if cv2.waitKey(1) & 0xff==ord('q'):
         break
 cam.release()
