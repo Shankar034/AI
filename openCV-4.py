9ec3e12269faa57f12571658975ecdf8e1afd885
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-20 16:52:18 +0545

    Multiple Windows and resizable openCV

--- openCV-4.py
+++ openCV-4.py
@@ -1,5 +1,8 @@
 import cv2
 
+rows = int (input("Boss, How many rows do you need?"))
+columns = int (input("And, how many columns do you need."))
+
 width =420
 height=360
 
@@ -11,11 +14,15 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(* 'MJPG'))
 
 while True:
     ignore, frame = cam.read()
-    cv2.imshow('Web-cam', frame)
-    cv2.moveWindow('Web-cam',10,20)
-    myFrame= cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('my-cam', myFrame)
-    cv2.moveWindow('my-cam',100,200)
+    
+    for i in range(0, rows):
+        for j in range(0,columns):
+            
+
+
+
+
     if cv2.waitKey(1) & 0xff==ord('q'):
         break
 cam.release();
+
