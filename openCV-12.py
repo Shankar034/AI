99dd215fa987e0d80769d761fe48c35514a4136e
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-31 22:30:25 +0545

    Region of interest

--- openCV-12.py
+++ openCV-12.py
@@ -3,24 +3,28 @@ import cv2
 height= 680
 width =460
 
+snipW=140
+snipH=90
+
+boxCC=int(width/2)
+boxRC=int(height/2)
 cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
 cam.set(cv2.CAP_PROP_FPS,30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
-rows =10
-columns=10
+
 while True:
     ignore, frame = cam.read()
-
-    colorB=frame[100:200,100:200]
-    colorB1=cv2.cvtColor(colorB,cv2.COLOR_BGR2GRAY)
-    grayF= cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    grayF[100:200,100:200]=colorB1
-    cv2.imshow("Frame1",grayF)
-    cv2.moveWindow("Frame1",10,10)
+    colorB=frame[int(boxRC-snipH/2):int(boxRC+snipH/2),int(boxCC-snipW/2):int(boxCC+snipW/2)]
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frame= cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
+    frame[int(boxRC-snipH/2):int(boxRC+snipH/2),int(boxCC-snipW/2):int(boxCC+snipW/2)]=colorB
     cv2.imshow("Frame2",colorB)
-    cv2.moveWindow("Frame2",700,100)
+    cv2.moveWindow("Frame2",height,0)
+    cv2.imshow("Frame1",frame)
+    cv2.moveWindow("Frame1",0,0)
+    
      
             
     if cv2.waitKey(1) & 0xff==ord('q'):
