899b0a42c24d83f5e2b84250fa3865b22ecb1e0c
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-07-31 16:16:21 +0545

    Region of interest

--- openCV-11.py
+++ openCV-11.py
@@ -15,12 +15,18 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 
 while True:
     ignore, frame= cam.read()
-    frameRoi=frame[170:310,280:440]
-    grayRoi =cv2.cvtColor(frameRoi,cv2.COLOR_BGR2GRAY);
+    frameRoi=frame[160:300,270:430]
+    grayRoi =cv2.cvtColor(frameRoi,cv2.COLOR_BGR2GRAY)
+    frameG2R=cv2.cvtColor(grayRoi,cv2.COLOR_GRAY2BGR)
+    # frame[0:140,70:230]=frameG2R
+    frame[0:140,70:230]=frameRoi
+    # frame[160:300,270:430]=frameG2R
     cv2.imshow("ROI frame",frameRoi)
     cv2.moveWindow("ROI frame",650,0)
     cv2.imshow("ROI gray",grayRoi)
     cv2.moveWindow("ROI gray",650,200)
+    cv2.imshow("ROI gray2bgr",frameG2R)
+    cv2.moveWindow("ROI gray2bgr",650,400)
     cv2.imshow("Webcam",frame)
     cv2.moveWindow("Webcam",0,0)
     if cv2.waitKey(1) & 0xff == ord('q'):
