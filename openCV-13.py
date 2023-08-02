634f7cca1517d31d11cba795a1bc0cc106cb6999
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-08-02 15:45:33 +0545

    Processing Mouse clicks and events

--- openCV-13.py
+++ openCV-13.py
@@ -1,5 +1,21 @@
 import cv2
 print(cv2.__version__)
+evt =0
+def mouseClick(event, xPos, yPos, flags, params):
+    global evt
+    global pvt
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(" The mouse Event was :",event)
+        print("The position is ", xPos,yPos)
+        evt = event
+        pvt = (xPos,yPos)
+    if event == cv2.EVENT_LBUTTONUP:
+        print(" The mouse Event was :",event)
+        print("The position is ",xPos,yPos)
+        evt = event
+       
+
+
 
 height= 460
 width = 680
@@ -9,9 +25,13 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
 cam.set(cv2.CAP_PROP_FPS,30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
+cv2.namedWindow("WebCam")
+cv2.setMouseCallback("WebCam",mouseClick)
 
 while True:
     ignore, frame = cam.read()
+    if evt==1:
+        cv2.circle(frame,pvt,30,(120,180,1),2)
     cv2.imshow("WebCam",frame)
     cv2.moveWindow("WebCam",10,10)
     if cv2.waitKey(1) & 0xff == ord('q'):
