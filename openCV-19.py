75febe71b77880101cadd3a7b36bb834957a18ab
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-08-19 22:15:54 +0545

    HSV color model

--- openCV-19.py
+++ openCV-19.py
@@ -1,10 +1,24 @@
 import cv2
 
-pos =0
-def onTrack1(event):
-    global pos
-    pos = event
 
+def onTrack1(pos):
+    global huelow
+    huelow = pos
+def onTrack2(pos):
+    global hueHigh
+    hueHigh = pos
+def onTrack3(event):
+    global SatLow
+    SatLow = event
+def onTrack4(event):
+    global SatHigh
+    SatHigh = event
+def onTrack5(event):
+    global ValueLow
+    ValueLow = event
+def onTrack6(event):
+    global ValueHigh
+    ValueHigh = event
 
 height = 460
 width = 580
@@ -17,12 +31,12 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 
 cv2.namedWindow("NewWin")
 cv2.moveWindow("NewWin",width,0)
-cv2.createTrackbar("low high", "NewWin",0,460,onTrack1)
-cv2.createTrackbar("low high", "NewWin",0,460,onTrack1)
-cv2.createTrackbar("low high", "NewWin",0,460,onTrack1)
-cv2.createTrackbar("low high", "NewWin",0,460,onTrack1)
-cv2.createTrackbar("low high", "NewWin",0,460,onTrack1)
-cv2.createTrackbar("low high", "NewWin",0,460,onTrack1)
+cv2.createTrackbar("hue low", "NewWin",10,179,onTrack1)
+cv2.createTrackbar("hue high", "NewWin",20,179,onTrack2)
+cv2.createTrackbar("sat low", "NewWin",20,255,onTrack3)
+cv2.createTrackbar("sat high", "NewWin",110,255,onTrack4)
+cv2.createTrackbar("val low", "NewWin",20,255,onTrack5)
+cv2.createTrackbar("low high", "NewWin",110,255,onTrack6)
 
 
 while True:
