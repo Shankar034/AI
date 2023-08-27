17ab014431fc6ef23d2dc96a45d576ce62fc023d
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-08-27 12:04:41 +0545

    Face recognition with openCV

--- openCV-25.py
+++ openCV-25.py
@@ -4,6 +4,12 @@ import face_recognition as FR
 font=cv2.FONT_HERSHEY_SIMPLEX
 donFace = FR.load_image_file('C:/Users/Dell/Documents/Python/demoimages/known/Donald Trump.jpg')
 faceloc = FR.face_locations(donFace)[0]
+donEncoding= FR.face_encodings(donFace)[0]
+
+shankarFace = FR.load_image_file('C:/Users/Dell/Documents/Python/demoimages/known/Shankar Bhandari.jpg')
+faceloc = FR.face_locations(donFace)[0]
+shankarEncoding= FR.face_encodings(donFace)[0]
+
 print(faceloc)
 top, right, buttom, left = faceloc
 cv2.rectangle(donFace,(left,top),(right,buttom),(0,255,255),3)
