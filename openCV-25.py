b665416f8c357deb9ee14147806a25fde7bd3344
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-08-27 11:51:35 +0545

    Face recognition with openCV

---
+++ openCV-25.py
@@ -0,0 +1,12 @@
+import cv2
+import face_recognition as FR
+
+font=cv2.FONT_HERSHEY_SIMPLEX
+donFace = FR.load_image_file('C:/Users/Dell/Documents/Python/demoimages/known/Donald Trump.jpg')
+faceloc = FR.face_locations(donFace)[0]
+print(faceloc)
+top, right, buttom, left = faceloc
+cv2.rectangle(donFace,(left,top),(right,buttom),(0,255,255),3)
+donFaceBGR=cv2.cvtColor(donFace, cv2.COLOR_RGB2BGR)
+cv2.imshow("My Bar", donFaceBGR)
+cv2.waitKey(5000)
