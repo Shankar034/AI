41ce76efb901a7cb310aceb1928e1fb575983e49
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-08-30 16:03:27 +0545

    installing mediapipe

--- openCV-30.py
+++ openCV-30.py
@@ -1,4 +1,5 @@
 import cv2
+import mediapipe as mp
 width = 460
 height=340
 cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -9,6 +10,7 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(* 'MJPG'))
 
 while True:
     ignore, frame = cam.read()
+    frame = cv2.resize(frame,(width,height))
     cv2.imshow('My-webcam', frame)
     cv2.moveWindow('My-webcam',100,100)
     
